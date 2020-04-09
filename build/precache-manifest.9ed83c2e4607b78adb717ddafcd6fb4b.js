a5c1532a88fc9e7e3e59939506de8920d45c78a8
Kumar Kaushik <kumarkaushik.tallam@nutanix.com>
2020-04-10 00:01:05 +0530

    second commit

---
+++ build/precache-manifest.9ed83c2e4607b78adb717ddafcd6fb4b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e665e11972ec1328a70dab2d0b349e2a",
+    "url": "./build/index.html"
+  },
+  {
+    "revision": "112b92c1ae6e13471368",
+    "url": "./build/static/css/main.836b475a.chunk.css"
+  },
+  {
+    "revision": "c703458b902d98bda040",
+    "url": "./build/static/js/2.96a85ac5.chunk.js"
+  },
+  {
+    "revision": "112b92c1ae6e13471368",
+    "url": "./build/static/js/main.43b94bfe.chunk.js"
+  },
+  {
+    "revision": "a95277f01883f9cb4248",
+    "url": "./build/static/js/runtime~main.c9f15127.js"
+  },
+  {
+    "revision": "cc1593745fa7a229f1b2f12fbeab7438",
+    "url": "./build/static/media/andhra_university.cc159374.jpg"
+  },
+  {
+    "revision": "c16c774055da41e2aea28938490389e1",
+    "url": "./build/static/media/andhra_university_1.c16c7740.jpg"
+  },
+  {
+    "revision": "1342c0b95928af05e4bfacd2c314aff8",
+    "url": "./build/static/media/arts_college.1342c0b9.jpg"
+  },
+  {
+    "revision": "7ca1223cd519211ed5077911bd4249f0",
+    "url": "./build/static/media/prawns.7ca1223c.jpg"
+  },
+  {
+    "revision": "453954e7c9df5b28af5b6d07efd1a2f1",
+    "url": "./build/static/media/prawns_1.453954e7.jpg"
+  },
+  {
+    "revision": "6d34cb3a97fa86081a1e12e4a279c10e",
+    "url": "./build/static/media/railway_bridge.6d34cb3a.jpg"
+  },
+  {
+    "revision": "0226c8da3a4da35568c7f445b018ca1c",
+    "url": "./build/static/media/vasavi.0226c8da.jpg"
+  },
+  {
+    "revision": "bb6ae457144f9e7844f6422b11d3b8c9",
+    "url": "./build/static/media/vasavi_1.bb6ae457.jpg"
+  },
+  {
+    "revision": "ef1995c8246eb4339599ecdc7fcc8b3f",
+    "url": "./build/static/media/vasavi_2.ef1995c8.jpg"
+  },
+  {
+    "revision": "3490fabe4de6d9bed0b68247fa70f293",
+    "url": "./build/static/media/vasavi_3.3490fabe.jpg"
+  },
+  {
+    "revision": "f4723a5d8ffdc402869b51e65b77cecd",
+    "url": "./build/static/media/vasavi_4.f4723a5d.jpg"
+  },
+  {
+    "revision": "7048728a8cb3c449e900a533cf2f0472",
+    "url": "./build/static/media/vasavi_business.7048728a.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ba63a63c76395a00ec0df888589dc88c.js"
+  "./build/precache-manifest.9ed83c2e4607b78adb717ddafcd6fb4b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
